ac7f2243f418b416e3995ca958b382544b5dccb2
Krish Parikh <85196467+Threqt1@users.noreply.github.com>
2024-01-16 17:05:03 -0600

    added animations

--- src/sprites/elf/minigame/teleporter.ts
+++ src/sprites/elf/minigame/teleporter.ts
@@ -8,8 +8,9 @@ export class ElfMinigameTeleporterNPC extends BaseNPC {
 
     constructor(scene: Phaser.Scene, x: number, y: number) {
         super(scene, x, y, 50, 50)
-        this.sprite = scene.physics.add.sprite(x, y, ElfTexture.TextureKey, ElfTexture.Animations.IdleFront)
+        this.sprite = scene.physics.add.sprite(x, y, ElfTexture.TextureKey)
         this.sprite.setPushable(false)
+        this.sprite.play(ElfTexture.Animations.IdleFront)
         this.updatePromptPosition(this.sprite)
     }
 
--- src/sprites/goblin/minigame/teleporter.ts
+++ src/sprites/goblin/minigame/teleporter.ts
@@ -8,8 +8,9 @@ export class GoblinMinigameTeleporterNPC extends BaseNPC {
 
     constructor(scene: Phaser.Scene, x: number, y: number) {
         super(scene, x, y, 50, 50);
-        this.sprite = scene.physics.add.sprite(x, y, GoblinTexture.TextureKey, GoblinTexture.Animations.IdleFront)
+        this.sprite = scene.physics.add.sprite(x, y, GoblinTexture.TextureKey)
         this.sprite.setPushable(false)
+        this.sprite.play(GoblinTexture.Animations.IdleFront)
         this.updatePromptPosition(this.sprite)
 
         GoblinTexture.configureGoblinPhysicsBody(this.sprite.body as Phaser.Physics.Arcade.Body)
