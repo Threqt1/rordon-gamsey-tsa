69aa3f03e538f8a7091ec6b18889c887732dbd3b
Krish Parikh <85196467+Threqt1@users.noreply.github.com>
2024-01-27 18:23:00 -0600

    added elf hub back

--- src/scenes/game/menu.ts
+++ src/scenes/game/menu.ts
@@ -51,7 +51,7 @@ export class MenuScene extends Phaser.Scene {
         startSprite.on("pointerup", () => {
             if (!switching) {
                 switching = true
-                fadeSceneTransition(this, SceneEnums.SceneNames.GoblinMinigame)
+                fadeSceneTransition(this, SceneEnums.SceneNames.ElfHub)
             }
         })
 
