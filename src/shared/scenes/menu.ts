6b803d3346c785125824bda0cece508264ebffc5
Krish Parikh <85196467+Threqt1@users.noreply.github.com>
2024-03-20 00:45:27 -0500

    final 3

--- src/shared/scenes/menu.ts
+++ src/shared/scenes/menu.ts
@@ -10,6 +10,8 @@ export class MenuScene extends Phaser.Scene {
     }
 
     preload() {
+        this.load.image("loading", "img/loading.png")
+
         this.anims.create({
             key: "startButton_animate",
             frames: this.anims.generateFrameNames("button", { start: 1, end: 7, prefix: "start_", suffix: ".png" }),
@@ -34,11 +36,22 @@ export class MenuScene extends Phaser.Scene {
             loop: true
         })
 
-        const cameraWidth = this.cameras.main.width
-        const cameraHeight = this.cameras.main.height
+        const camera = this.cameras.main;
+        const cameraWidth = camera.width
+        const cameraHeight = camera.height
 
-        const startSprite = this.add.sprite(cameraWidth / 2, cameraHeight / 2, "button", "start_1.png")
-        const buttonScale = Math.max(cameraWidth / startSprite.width, cameraHeight / startSprite.height) / 10
+        let image = this.add.image(camera.centerX, camera.centerY, "loading")
+        image.setScale(Math.min(camera.width / image.width, camera.height / image.height))
+        //image.setScale(Math.max(cameraWidth / image.width, cameraHeight / image.height))
+        // Set the zoom based on the smallest dimension
+        // if (this.scale.height > this.scale.width) {
+        //     camera.setZoom(this.scale.width / image.width)
+        // } else {
+        //     camera.setZoom(this.scale.height / image.height)
+        // }
+
+        const startSprite = this.add.sprite(camera.centerX, camera.centerY + image.displayHeight / 3, "button", "start_1.png")
+        const buttonScale = Math.max(cameraWidth / startSprite.width, cameraHeight / startSprite.height) / 15
         startSprite.setScale(buttonScale)
         startSprite.setInteractive()
 
@@ -60,7 +73,7 @@ export class MenuScene extends Phaser.Scene {
         startSprite.on("pointerup", () => {
             if (!switching) {
                 switching = true
-                SceneUtil.fadeSceneTransition(this, SceneEnums.Name.Menu)
+                SceneUtil.fadeSceneTransition(this, SceneEnums.Name.ElfHub)
             }
         })
 
