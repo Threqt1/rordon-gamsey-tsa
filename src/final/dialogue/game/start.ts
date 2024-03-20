a30bfb5bafda3033a85816aa51c020bfb0583b1e
Krish Parikh <85196467+Threqt1@users.noreply.github.com>
2024-03-20 10:46:39 -0500

    add dialogue final 5

--- src/final/dialogue/game/start.ts
+++ src/final/dialogue/game/start.ts
@@ -10,7 +10,7 @@ let Base: DialogueSystem.Dialogue = {
             "And so, with the Sacred Apple, Special Sauce, and... Illegal Sausages? The player cooked.",
             "And cooked some more.",
             "And as he completed his masterpiece...",
-            "The deadline for Rordon Gamsey's holy feeding ceremony arrived.",
+            "The deadline for Rordon Gamsey's holy ceremony of food arrived.",
             "Representatives from all tribes gathered to feed the ruler himself."
         ]
     },
